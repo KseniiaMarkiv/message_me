fff5b0e00adf0b48bf55d16e6ca7cafb8cae3fa3
KseniiaMarkiv <ksenya.markiv@gmail.com>
2021-11-10 18:44:02 +0200

    modify js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,12 @@
 //= require semantic-ui
 //= require_tree .
 
+scroll_bottom = function() {
+    if ($('#messages').length > 0) {
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    }
+}
+
 submit_message = function() {
     $('#message_body').on('keydown', function(e) {
         if (e.keyCode == 13) {
@@ -26,11 +32,6 @@ submit_message = function() {
     });
 };
 
-scroll_bottom = function() {
-    if ($('#messages').length > 0) {
-        $('#messages').scrollTop($('#messages')[0].scrollHeight);
-    }
-}
 $(document).on('turbolinks:load', function() {
     $('.ui.dropdown').dropdown();
     $('.message .close').on('click', function() {
